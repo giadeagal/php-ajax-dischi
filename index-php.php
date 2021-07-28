a64e03c402fef00f843d1826ab77d396113420e9
Gianluca <gl.galassini@gmail.com>
2021-07-28 17:00:04 +0200

    fine milestone 1 con layout

--- index-php.php
+++ index-php.php
@@ -16,23 +16,36 @@ include "db.php";
 </head>
 <body>
 
-<?php foreach ($albums as $album) {?>
-    <div class="card" style="width: 18rem;">
-  <img src="<?php echo $album['poster']?>" class="card-img-top" alt="<?php echo $album['title'] ?>">
-  <div class="card-body">
-    <h5 class="card-title"><?php echo $album['title'] ?></h5>
-    <ul class="card-text">
-        <li>
-            <strong>Autore: </strong> <span><?php echo $album['author'] . ";"?></span>
-        </li>
-        <li>
-            <strong>Anno: </strong> <span><?php echo $album['year']?></span>
-        </li>
-    </ul>
-  </div>
-</div>        
-<?php } ?>
+<header>
+    <a href="#">
+        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" alt="">
+    </a>
+</header>
 
+<main>
+
+<div class="container">
+    <div class="row justify-content-evenly">
+    <?php foreach ($albums as $album) {?>
+    <div class="card col-2 m-3" >
+        <img src="<?php echo $album['poster']?>" class="card-img-top" alt="<?php echo $album['title'] ?>">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $album['title'] ?></h5>
+            <ul class="card-text">
+                <li>
+                    <strong>Autore: </strong> <span><?php echo $album['author'] . ";"?></span>
+                </li>
+                <li>
+                    <strong>Anno: </strong> <span><?php echo $album['year']?></span>
+                </li>
+            </ul>
+        </div>
+    </div>        
+<?php } ?>
+    </div>
+</div>
+
+</main>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
