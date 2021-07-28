822ebf6509b721ae1694b0bf4a4f5436562547b0
Gianluca <gl.galassini@gmail.com>
2021-07-28 13:28:26 +0200

    init e import database

---
+++ db.php
@@ -0,0 +1,66 @@
+<?php 
+
+$albums = [
+    [
+        'title' => 'Kind of Blue',
+        'author' => 'Miles Davis',
+        'year' => 1959,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/9/9c/MilesDavisKindofBlue.jpg'
+    ],
+    [
+        'title' => 'A Love Supreme',
+        'author' => 'John Coltrane',
+        'year' => 1965,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/9/9a/John_Coltrane_-_A_Love_Supreme.jpg'
+    ],
+    [
+        'title' => 'The Shape of Jazz to Come',
+        'author' => 'Ornette Coleman',
+        'year' => 1959,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/c/c9/ShapeOfJazzToCome.jpg'
+    ],
+    [
+        'title' => 'Sunday at the Village Vanguard',
+        'author' => 'Bill Evans Trio',
+        'year' => 1961,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/a/a1/Sunday_at_the_Village_Vanguard.jpg'
+    ],
+    [
+        'title' => 'Saxophone Colossus',
+        'author' => 'Sonny Rollins',
+        'year' => 1956,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/f/ff/Saxophone_Colossus_-_Sonny_Rollins.jpg'
+    ],
+    [
+        'title' => 'Brilliant Corners',
+        'author' => 'Thelonious Monk',
+        'year' => 1957,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/a/aa/BrilliantCornersTheloniousMonk.jpg'
+    ],
+    [
+        'title' => 'Mingus Ah Um',
+        'author' => 'Charles Mingus',
+        'year' => 1959,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/6/65/Mingus_Ah_Um_-_Charles_Mingus.jpg'
+    ],
+    [
+        'title' => 'Bitches Brew',
+        'author' => 'Miles Davis',
+        'year' => 1970,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/7/72/Bitches_brew.jpg'
+    ],
+    [
+        'title' => 'The Köln Concert',
+        'author' => 'Keith Jarrett',
+        'year' => 1975,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/f/fa/Keith_Jarrett_Koln_Concert_Cover.jpg'
+    ],
+    [
+        'title' => 'Giant Steps',
+        'author' => 'John Coltrane',
+        'year' => 1960,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/2/2a/Coltrane_Giant_Steps.jpg'
+    ],
+];
+
+?>
---
+++ index-php.php
@@ -0,0 +1,18 @@
+<?php 
+
+include "db.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Top 10 Jazz Albums of All Time</title>
+</head>
+<body>
+    <img src="<?php echo $albums[0]['poster']?>" alt="">
+</body>
+</html>
